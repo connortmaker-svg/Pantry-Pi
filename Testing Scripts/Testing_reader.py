fea058631c3539e2a95d4eafb94fce9ea7e16113
Connor The Maker <connortmaker@gmail.com>
2026-02-28 13:26:34 -0500

    checking Logic on Barcode Acceptance

--- Testing Scripts/Testing_reader.py
+++ Testing Scripts/Testing_reader.py
@@ -2,6 +2,8 @@ while True:
     try:
         scan = input("Scan the Barcode: ")
         if scan.strip():
+            if scan == "0613008756451":
+                print("Arizona")
             print(f"Barcode scanned: {scan}")
         else:
             print("No barcode scanned.")
