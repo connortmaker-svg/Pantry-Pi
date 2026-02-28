dd8176d57f5eded47110bd30e0e542d03e8e28b0
Connor The Maker <connortmaker@gmail.com>
2026-02-28 14:23:27 -0500

    Functions created for loading data, adding data, and saving data to the json file

--- Testing Scripts/Testing_reader.py
+++ Testing Scripts/Testing_reader.py
@@ -1,26 +1,53 @@
 import json
 
-try:
-    with open('Testing Scripts/data.json', 'r') as file:
-        inventory = json.load(file)
-except FileNotFoundError:
-    print("Error: 'database_t.json' not found! Make sure it's in the same folder.")
-    inventory = {}
+FILE_PATH = 'Testing Scripts/data.json'
+
+
+def load_data():
+    try:
+        with open(FILE_PATH, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+def save_data(data):
+    with open(FILE_PATH, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def add_new_item(barcode):
+    print(f"/n Adding New Product: {barcode}")
+    name = input("Item Name:").strip()
+    price = input("Price: ").strip()
+    qty = input("Quantity:").strip()
+    
+    inventory[barcode] = {
+        "name" : name,
+        "price" : price,
+        "stock" : qty
+    }
+
+    save_data(inventory)
+    print("Item Saved into Inventory")
+    
+inventory = load_data()
 
 while True:
     try:
-        scan = input("Scan the Barcode: ").strip()
-        if scan:
-            if scan in inventory:
-                item = inventory[scan]
-                print(f"--- Item Found ---")
-                print(f"Product: {item['name']}")
-                print(f"Price:   ${item['price']}")
-                print(f"Stock:   {item['stock']} units")
-            else:
-                print(f"Unknown Barcode: {scan}")
+        scan = input("Start Scanning a Barcode:  ").strip()
+        if not scan:
+            continue
+        if scan.lower() == 'q':
+            break
+
+        if scan in inventory:
+            item = inventory[scan]
+            print(f"Product: {item['name']}")
+            print(f"Price:   ${item['price']}")
+            print(f"Stock:   {item['stock']} units\n")
         else:
-            print("No input detected.")
+            print(f"Barcode {scan} not found in inventory")
+            choice = input("Would you Life to add it now? (y/n): ").lower()
+            if choice == 'y':
+                add_new_item(scan)
     except KeyboardInterrupt:
-        print("\nExiting...")
         break
